7fec8126d41fa584ef3baef73b3cf8ad2d1a44b6
widjoyono <71638181+widjoyono@users.noreply.github.com>
2021-07-15 05:03:57 +0700

    Update mathematicalExpressions.java

--- src/mathematicalExpressions.java
+++ src/mathematicalExpressions.java
@@ -18,6 +18,7 @@ public class mathematicalExpressions {
         operand1 = 5;
         operand2 = 7;
         operand3 = operand2;
+
         // addition
 
         result = 42 + 53;
@@ -27,12 +28,38 @@ public class mathematicalExpressions {
         result = 2;
         result += 4;
         result += operand1;
-
         result = 9 - 5;
-
         result = operand1 - operand2;
+        result = 15 - operand1;
+        result = 0;
+        result = 2;
 
-        result =
+        //  substraction
+        result -= 4;
+        result -= operand1;
+        result -= operand1 - operand2 - operand3;
+
+        // multiplication
+
+        result = 0;
+        result = operand1 * operand2 * operand3;
+        result *= 2;
+
+        // division and modulus
+        result /= 2;
+        result /= 2;
+        result /= operand1;
+        result /= 3;
+        result /= 3;
+        // this variable turn integer 2 but if use float it turn 2.6
+
+        result = operand1 % operand2;
+        result = 100 % 7;
+
+        // postfix and prefix operators
+
+        operand1 = 10; // it changes variable from 5 to 10
+        result = ++operand1;
 
         System.out.println(result);
     }
