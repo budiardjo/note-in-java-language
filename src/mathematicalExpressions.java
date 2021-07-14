824e264f596d647e7560d63aa61a1b9bee641e99
widjoyono <71638181+widjoyono@users.noreply.github.com>
2021-07-15 04:26:30 +0700

    Initial commit

---
+++ src/mathematicalExpressions.java
@@ -0,0 +1,39 @@
+public class mathematicalExpressions {
+    public static void main (String[] args) {
+
+
+        // declare variable
+
+        int result;
+        int operand1;
+        int operand2;
+        int operand3;
+        int operand4;
+
+        // now result has the value of 0
+
+        result = 0;
+
+        // initialize the operands
+        operand1 = 5;
+        operand2 = 7;
+        operand3 = operand2;
+        // addition
+
+        result = 42 + 53;
+        result = operand1 + operand2;
+        result = 1 + operand1;
+        result = 1 + operand1 + operand2 + operand3;
+        result = 2;
+        result += 4;
+        result += operand1;
+
+        result = 9 - 5;
+
+        result = operand1 - operand2;
+
+        result =
+
+        System.out.println(result);
+    }
+}
