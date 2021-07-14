a955aba907d33a045416728efc1dd44523d6ec5f
widjoyono <71638181+widjoyono@users.noreply.github.com>
2021-07-15 05:19:16 +0700

    adding new file

---
+++ src/BucketsOfFun.java
@@ -0,0 +1,25 @@
+public class BucketsOfFun {
+    public static void main (String[] args) {
+
+        // DECLARE ALL THE THINGS
+        int butterflies;
+        int beetles;
+        int bugs;
+
+        // NOW GIVE THEM SOME VALUES
+
+        butterflies = 5;
+        beetles = 9;
+
+        bugs = butterflies + beetles;
+        System.out.println("there are only " + butterflies + " butterflies,");
+        System.out.println("but there are  " + bugs + " bugs in all.");
+
+        System.out.println(" NOOOOO, my dog ate one.");
+        butterflies--;
+        System.out.println("Now there are only " + butterflies + " butterflies left");
+        System.out.println("But there are still " + bugs + " bugs left!!");
+        System.out.println("Wait a minute!");
+        System.out.println(" .......maybe my can't do math after all");
+    }
+}
